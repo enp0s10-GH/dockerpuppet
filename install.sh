8da0bc2eaef04bee419c74d50debc2b5af636093
Vincent Roch <vincent.roch@hetzner.com>
2021-11-11 11:49:59 +0100

    formatting

--- install.sh
+++ install.sh
@@ -2,26 +2,45 @@
 #
 # This Script creates an docker container, and installs an puppetserver the container.
 
-##############################
+#######################################
+# function to install packages
+# Arguments:
+#   $1: package name
+# Returns:
+#   apt install $1
+#######################################
+function install() {
+  package="${1}"
+  apt install "${package}" -y
+}
+
+#######################################
 # Adding hostname puppet
-##############################
+# Output:
+#   Overwrites the /etc/hosts   
+#######################################
 function handle_host() {
-  ipcfg=$(ifconfig | awk '/inet/{print $2}');
-  IFS=" ";
-  read -ra addr <<< "${ipcfg}";
-  for ip_address in ${addr[0]}; do
+  local ipcfg="$(ifconfig | awk '/inet/{print $2}')"
+  IFS=' '
+  read -ra addr <<< "${ipcfg}"
+  for ip_address in "${addr[0]}"; do
     hostname=$(cat /etc/hostname)
     echo "${hostname} puppet" > /etc/hostname
-    echo "127.0.0.1   localhost puppet" > /etc/hosts
-    echo "$ip_address    ${hostname}" >> /etc/hosts
-  done;
+    echo '127.0.0.1   localhost puppet' > /etc/hosts
+    echo "${ip_address}    ${hostname}" >> /etc/hosts
+  done
 }
-handle_host;
 
-##############################
-# Installing & configuring puppetserver
-##############################
-apt-get install puppetserver -y
+#######################################
+# Installing, Configuring and starting of the puppetserver.
+# Globals:
+#   PATH
+#   SERVERCERT 
+# Output:
+#   Writes 2 paths to .bashrc
+#######################################
+function setup() {
+install puppetserver
 echo "export PATH=$PATH:/opt/puppetlabs/bin:/opt/puppetlabs/server/apps/puppetserver/bin" >> ~/.bashrc
 source /root/.bashrc
 apt-get update -y && apt-get upgrade -y
@@ -29,5 +48,9 @@ rm -rf /etc/puppetlabs/puppetserver/ca
 rm -rf /etc/puppetlabs/puppet/ssl
 /opt/puppetlabs/server/apps/puppetserver/bin/puppetserver ca setup --subject-alt-names "${SERVERCERT}",puppet
 /opt/puppetlabs/server/apps/puppetserver/bin/puppetserver foreground
+}
+
+handle_host
+setup
 
 
