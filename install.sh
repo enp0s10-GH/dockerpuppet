c606170af71f25c8ba39fb76cfc12117ceaaa538
Vincent Roch <vincent.roch@hetzner.com>
2021-11-11 11:16:34 +0100

    shellcheck

--- install.sh
+++ install.sh
@@ -5,31 +5,29 @@
 ##############################
 # Adding hostname puppet
 ##############################
-ipcfg=$(ifconfig | awk '/inet/{print $2}');
-IFS=" ";
-read -ra ADDR <<< $ipcfg;
-for ip in ${ADDR[0]}; do
-HOSTNAME=$(cat /etc/hostname)
-echo "${HOSTNAME} puppet" > /etc/hostname
-echo "127.0.0.1   localhost puppet" > /etc/hosts
-echo "$ip    ${HOSTNAME}" >> /etc/hosts
-done;
+function handle_host() {
+  ipcfg=$(ifconfig | awk '/inet/{print $2}');
+  IFS=" ";
+  read -ra addr <<< "${ipcfg}";
+  for ip_address in ${addr[0]}; do
+    hostname=$(cat /etc/hostname)
+    echo "${hostname} puppet" > /etc/hostname
+    echo "127.0.0.1   localhost puppet" > /etc/hosts
+    echo "$ip_address    ${hostname}" >> /etc/hosts
+  done;
+}
+handle_host;
 
 ##############################
 # Installing & configuring puppetserver
 ##############################
-$server=$SERVERCERT
-echo "________"
-echo $server;
-echo "________"
 apt-get install puppetserver -y
 echo "export PATH=$PATH:/opt/puppetlabs/bin:/opt/puppetlabs/server/apps/puppetserver/bin" >> ~/.bashrc
-source ~/.bashrc
+source /root/.bashrc
 apt-get update -y && apt-get upgrade -y
 rm -rf /etc/puppetlabs/puppetserver/ca
 rm -rf /etc/puppetlabs/puppet/ssl
-										 # ENTER YOUR PUPPETSERVER NAME
-/opt/puppetlabs/server/apps/puppetserver/bin/puppetserver ca setup --subject-alt-names ${SERVERCERT},puppet
+/opt/puppetlabs/server/apps/puppetserver/bin/puppetserver ca setup --subject-alt-names "${SERVERCERT}",puppet
 /opt/puppetlabs/server/apps/puppetserver/bin/puppetserver foreground
 
 
