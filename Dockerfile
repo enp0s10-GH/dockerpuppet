b93010eb689767447a3dd178555614b3320abfe8
Vincent Roch <vincent.roch@hetzner.com>
2021-11-11 09:29:17 +0100

    stable

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,22 @@
+# Using Image Ubuntu:20.04
 FROM ubuntu:20.04
-COPY install.sh . 
-#ENTRYPOINT ["/bin/sh", "-c" , "echo 172.17.0.2   dockerpuppet puppet >> /etc/hosts && echo dockerpuppet puppet > /etc/hostname" ]
-ENTRYPOINT ["bash","/install.sh"]
+
+# Copying the local install.sh into the container
+COPY install.sh .
+ 
+# Updating OS and installing dependencies
+RUN apt update -y && apt upgrade -y; \
+    apt install wget -y; \
+    wget https://apt.puppetlabs.com/puppet-release-focal.deb; \
+    dpkg -i puppet-release-focal.deb; \
+    apt install perl -y; \
+    apt install net-tools -y; \
+    apt-get update -y; \
+    apt -y -o DPkg::Options::=--force-confold install puppet-agent;
+
+# Exposing Port 8140
 EXPOSE 8140
+
+# Execute install.sh at start of the container
+ENTRYPOINT ["bash","/install.sh"]
+
--- install.sh
+++ install.sh
@@ -2,42 +2,29 @@
 #
 # This Script creates an docker container, and installs an puppetserver the container.
 
-apt update -y && apt upgrade -y
-##############################
-# installing some dependencies
-##############################
-apt install wget -y
-wget https://apt.puppetlabs.com/puppet-release-focal.deb
-dpkg -i puppet-release-focal.deb
-apt install perl -y
-apt install net-tools -y
-apt-get update -y
 ##############################
 # Adding hostname puppet
 ##############################
+ipcfg=$(ifconfig | awk '/inet/{print $2}');
+IFS=" ";
+read -ra ADDR <<< $ipcfg;
+for ip in ${ADDR[0]}; do
 HOSTNAME=$(cat /etc/hostname)
 echo "${HOSTNAME} puppet" > /etc/hostname
 echo "127.0.0.1   localhost puppet" > /etc/hosts
-echo "::1   localhost ip6-localhost ip6-loopback" >> /etc/hosts
-echo "fe00::0   ip6-localnet" >> /etc/hosts
-echo "ff00::0   ip6-mcastprefix" >> /etc/hosts
-echo "ff02::1   ip6-allnodes" >> /etc/hosts
-echo "ff02::2   ip6-allrouters" >> /etc/hosts
-echo "172.17.0.2     ${HOSTNAME}" >> /etc/hosts
-##############################
-# Installing & configuring puppet
-##############################
-# apt purge puppet -y
-# apt autoremove -y
-# apt install puppet -y 
+echo "$ip    ${HOSTNAME}" >> /etc/hosts
+done;
+
 ##############################
 # Installing & configuring puppetserver
 ##############################
 apt-get install puppetserver -y
+echo "export PATH=$PATH:/opt/puppetlabs/bin:/opt/puppetlabs/server/apps/puppetserver/bin" >> ~/.bashrc
+source ~/.bashrc
 apt-get update -y && apt-get upgrade -y
 rm -rf /etc/puppetlabs/puppetserver/ca
 rm -rf /etc/puppetlabs/puppet/ssl
-PATH=$PATH:/opt/puppetlabs/bin:/opt/puppetlabs/server/apps/puppetserver/bin
+										 # ENTER YOUR PUPPETSERVER NAME
 /opt/puppetlabs/server/apps/puppetserver/bin/puppetserver ca setup --subject-alt-names puppetserver.hetzner.company,puppet
 /opt/puppetlabs/server/apps/puppetserver/bin/puppetserver foreground
 
